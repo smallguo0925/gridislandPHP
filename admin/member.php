f7fb9816ee9c0d7c6b3c1286b143237a8838b56f
morxdd <pomin1995qoot@gmail.com>
2024-02-06 16:44:38 +0800

    會員管理php

--- admin/member.php
+++ admin/member.php
@@ -1,3 +1,28 @@
-<?
-// gtvjsdfgjdfsghjfdshkgfdshkgfdshkfsdkh
+<?php
+//跨域資源共享設定，允許所有來源訪問
+header("Access-Control-Allow-Origin: *");
+//允許使用的 HTTP 方法
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+//允許的請求標頭
+header("Access-Control-Allow-Headers: Content-Type");
+//設定回應類型為 JSON
+header("Content-Type: application/json");
+
+try {
+    // 引入資料庫連接設定檔案
+    require_once("../GridIsland/connectGridIsland.php");
+    // 準備從 mem 表中選擇所有記錄的 SQL 查詢
+    $sql = "SELECT * FROM mem";
+    // 執行 SQL 查詢，返回 PDOStatement 物件
+    $members = $pdo->query($sql);
+    // 從 PDOStatement 物件中取得所有記錄，以關聯陣列格式
+    $memberRows = $members->fetchAll(PDO::FETCH_ASSOC);
+    // 準備要回傳的數據，包括錯誤狀態、訊息和會員資料
+    $result = ["error" => false, "msg" => "", "mem" => $memberRows];
+} catch (PDOException $e) {
+    // 捕捉到 PDOException，設置錯誤狀態和錯誤訊息
+    $result = ["error" => true, "msg" => $e->getMessage()];
+}
+// 將結果編碼為 JSON 格式並輸出
+echo json_encode($result);
 ?>
