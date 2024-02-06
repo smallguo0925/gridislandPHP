9815fcdaad0dfc368916d4affee9aa5c579917f0
smallguo0925 <l09250701@gmail.com>
2024-02-06 12:00:14 +0800

    init

---
+++ connectGridIsland.php
@@ -0,0 +1,12 @@
+<?php 
+	$dbname = `grid_island`;//資料庫名稱
+	$user = "root";//使用者帳號
+	$password = "";//使用者密碼
+	$port = 3306;//連接的port
+	$dsn = "mysql:host=localhost;port={$port}};dbname=$dbname;charset=utf8";
+
+	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
+
+	//建立pdo物件
+	$pdo = new PDO($dsn, $user, $password, $options);
+?>
