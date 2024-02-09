55fda0f1f0dd61a40b8e5ba784b12284963a2a0d
smallguo0925 <l09250701@gmail.com>
2024-02-09 14:00:43 +0800

    微修桌型PHP的名字跟嘗試會員中心getMember資料

--- front/getMember.php
+++ front/getMember.php
@@ -1,31 +1,52 @@
-<?
-//需請教董老師，上線後是否把以下註解
+<?php
 //跨域的設定
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+
 header("Content-Type: application/json");
-//好像是檔案接受的格式
+
+session_start(); //用session來記錄登入的狀況
 
 
 try {
 	//連線到剛建立的connect檔
     require_once("../connectGridIsland.php");
 
-  //準備sql指令，我要拿table_type裡全部的資料，到時候有帳號密碼的檔案不能這樣拿。
-	$sql = "SELECT * FROM `table_type`";
+  //準備sql指令，要拿table_type裡全部的資料。
+	$sql = "SELECT * FROM `mem`";
 
   // 建立PDO Statement，原本的寫法會是
     //   $pdoStatement = $pdo->query($sql);
-	//為方便使用閱讀，將$pdoStatement設定為$table_type
-  $table_type = $pdo->query($sql);
+	//為方便使用閱讀，將$pdoStatement設定為$mem
+  $mem = $pdo->query($sql);
 	//因為前面用select，這裡用query即可
 
-  $table_typeRows = $table_type->fetchAll(PDO::FETCH_ASSOC);
-	$result = ["error" => false, "msg" => "", "table_type" => $table_typeRows];
+  $memRows = $mem->fetchAll(PDO::FETCH_ASSOC);
+	$result = ["error" => false, "msg" => "", "mem" => $memRows];
 } catch (PDOException $e) {
     $result = ["error" => true, "msg" => $e->getMessage()];
 }
 echo json_encode($result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 ?>
