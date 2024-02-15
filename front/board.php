f9f45bec45714a6fa7e5e600a0ccfb2c5bab3814
zhuuu98 <liao.yingzhu98@gmail.com>
2024-02-15 15:23:57 +0800

    前台留言版php

--- front/board.php
+++ front/board.php
@@ -10,13 +10,38 @@ try {
 	//連線到剛建立的connect檔
   require_once("../../GridIsland/connectGridIsland.php");
 
-  $sql = "select * from msg";
+  $sql = "SELECT 
+  ms.msg_id, ms.mem_id, ms.msg_content, ms.msg_state, ms.msg_datetime,
+  me.mem_name, me.mem_nickname, 
+  (
+    select
+      JSON_ARRAYAGG(
+        JSON_OBJECT(
+        'reply_id', re.reply_id,
+        'reply_content', re.reply_content,
+        'reply_time', re.reply_time,
+              'reply_memName', me.mem_name
+        )
+          )
+          from reply re
+          join mem me 
+          on me.mem_id = re.mem_id
+          where re.msg_id = ms.msg_id
+          )as replies
+  from msg ms 
+  join mem me on ms.mem_id = me.mem_id;";
 
   // 建立PDO Statement，原本的寫法會是$pdoStatement = $pdo->query($sql);
   $board = $pdo->query($sql);
   //沒有未知數(前台傳進來的資料)用query就好
 
   $boardRows = $board->fetchAll(PDO::FETCH_ASSOC);
+
+  // 對於每條消息的回覆數據進行解析
+  // foreach ($boardRows as &$row) {
+  //   $row['replies'] = json_decode($row['replies'], true);
+  // }
+
 	$result = ["error" => false, "errorMessage" => "", "board" => $boardRows];
 } catch (PDOException $e) {
     $result = ["error" => true, "errorMessage" => $e->getMessage()];
