fd6ebab2c6cb3e159aef7d5c38d1fcecf5c240cf
martina216 <10230260c@gmail.com>
2024-02-19 21:00:12 +0800

    僅測試

--- connectGridIsland.php
+++ connectGridIsland.php
@@ -5,6 +5,7 @@
 	// $password = "root";//使用者密碼for Mac
 	$port = 3306;//連接的port
 	$dsn = "mysql:host=localhost;port=$port;dbname=$dbname;charset=utf8";
+
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
 
 	//建立pdo物件
