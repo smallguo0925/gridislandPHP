7135920fa36826fac17733bfaf4d6af4a22f86be
martina216 <10230260c@gmail.com>
2024-02-19 21:05:56 +0800

    檔案覆蓋

--- connectGridIsland.php
+++ connectGridIsland.php
@@ -5,20 +5,7 @@
 	// $password = "password";//使用者密碼for Mac
 	$port = 3306;//連接的port
 	$dsn = "mysql:host=localhost;port=$port;dbname=$dbname;charset=utf8";
-
-<<<<<<< HEAD
 	
-
-	// $dbname = "tibamefe_chd104g5";
-	// $user = "tibamefe_since2021";
-	// $password = "vwRBSb.j&K#E";
-	// $port = 3306;
-	// $dsn = "mysql:host=localhost;port=$port;dbname=$dbname;charset=utf8";
-
-	
-	
-=======
->>>>>>> 4c43863c6f0c444fbf76e0f452e823b5b66ae0e2
 	$options = array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION, PDO::ATTR_CASE=>PDO::CASE_NATURAL);
 
 	//建立pdo物件
