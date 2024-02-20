43b376d424513647d2924b31489252a675de2760
martina216 <10230260c@gmail.com>
2024-02-21 00:00:22 +0800

    前台優惠碼.php 更改

---
+++ front/getCupponCode.php
@@ -0,0 +1,17 @@
+<?php
+require_once("../header.php");
+
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+try {
+    require_once("../connectGridIsland.php");
+	$sql = "SELECT * from promo where promo_state = 1";
+    $promos = $pdo->query($sql);
+    $promosRow = $promos->fetchAll(PDO::FETCH_ASSOC);
+	$result = ["error" => false, "msg" => "成功取得優惠碼資料", "promos" => $promosRow];
+} catch (PDOException $e) {
+    $result = ["error" => true, "msg" => $e->getMessage()];
+}
+echo json_encode($result);
+?>
