aa763b6a63b25777ac20e30858414542cf1729a3
zhuuu98 <liao.yingzhu98@gmail.com>
2024-02-21 11:15:25 +0800

    檢舉後台完成

--- admin/report.php
+++ admin/report.php
@@ -1,22 +1,20 @@
 <?php
-//跨域的設定
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-header("Content-Type: application/json");
-
 
 try {
-	//連線到剛建立的connect檔
+  //跨域的設定
+  header("Access-Control-Allow-Origin: *");
+  header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+  header("Access-Control-Allow-Headers: Content-Type");
+  header("Content-Type: application/json");
+  //連線到connect檔
   require_once("../../GridIsland/connectGridIsland.php");
 
   $sql = "select r.report_id, m.msg_id, r.report_reason, m.msg_content, r.report_state, r.report_check 
     FROM report r join msg m on r.msg_id = m.msg_id
-    ORDER BY msg_id";
+    ORDER BY msg_id desc";
 
   // 建立PDO Statement，原本的寫法會是$pdoStatement = $pdo->query($sql);
   $report = $pdo->query($sql);
-  //沒有未知數(前台傳進來的資料)用query就好
 
   $reportRows = $report->fetchAll(PDO::FETCH_ASSOC);
 	$result = ["error" => false, "errorMessage" => "", "Report" => $reportRows];
